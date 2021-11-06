bb99b5ca12bc3b8dc91b5125931636bb6fe612a1
Arumarov <arumarov@yandex.ru>
2021-11-06 17:44:27 +0600

    Add Task00

---
+++ Task00.Intern/Program.cs
@@ -0,0 +1,30 @@
+﻿// // ЗАДАЧА 00. Вывести квадрат числа
+ 
+// // Простое решение задачи
+// int a = 5;
+// Console.WriteLine(a*a);
+
+
+
+// // Решение при помощи метода
+// int Square (int x)
+// {
+//     return (x*x);
+// }
+
+// int a = 5;
+// Console.WriteLine(Square(a));
+
+// // Решение от преподавателя
+// Console.WriteLine(Math.Pow(5,2));
+
+
+// // Второе решение от преподавателя
+// double a = 5;
+// Console.WriteLine(Math.Pow(a,2));
+
+// Третье решение от преподавателя (ввод с клавиатуры)
+double a;
+string s = Console.ReadLine();
+a = Convert.ToInt32(s);
+Console.WriteLine(Math.Pow(a,2));
