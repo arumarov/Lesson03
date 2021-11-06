acf5cf56e7961ef764815a1ab3476288f8b16aa0
Arumarov <arumarov@yandex.ru>
2021-11-06 20:13:51 +0600

    Add Task02

---
+++ Task02.Intern/Program.cs
@@ -0,0 +1,29 @@
+﻿// ЗАДАЧА 02. Даны 2 числа. Показать большее и меньшее число
+int max (int a, int b)
+
+{
+    int max = a;
+    if (b > a) 
+    {
+        max = b;
+    }
+    return max;
+}
+
+int min (int a, int b)
+
+{
+    int min = a;
+    if (b < a) 
+    {
+        min = b;
+    }
+    return min;
+}
+
+int a = 250;
+int b = 88;
+Console.Write("Максимальное число: ");
+Console.WriteLine(max(a, b));
+Console.Write("Минимальное число: ");
+Console.WriteLine(min(a, b));
