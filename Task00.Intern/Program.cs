b9df36c8ffdaed57b973b244bfa8c596bd253bfb
Arumarov <arumarov@yandex.ru>
2021-11-06 18:46:40 +0600

    Add Task01

--- Task00.Intern/Program.cs
+++ Task00.Intern/Program.cs
@@ -1,27 +1,27 @@
-﻿// // ЗАДАЧА 00. Вывести квадрат числа
+﻿// ЗАДАЧА 00. Вывести квадрат числа
  
-// // Простое решение задачи
-// int a = 5;
-// Console.WriteLine(a*a);
+// Простое решение задачи
+int a = 5;
+Console.WriteLine(a*a);
 
 
 
-// // Решение при помощи метода
-// int Square (int x)
-// {
-//     return (x*x);
-// }
+// Решение при помощи метода
+int Square (int x)
+{
+    return (x*x);
+}
 
-// int a = 5;
-// Console.WriteLine(Square(a));
+int a = 5;
+Console.WriteLine(Square(a));
 
-// // Решение от преподавателя
-// Console.WriteLine(Math.Pow(5,2));
+// Решение от преподавателя
+Console.WriteLine(Math.Pow(5,2));
 
 
-// // Второе решение от преподавателя
-// double a = 5;
-// Console.WriteLine(Math.Pow(a,2));
+// Второе решение от преподавателя
+double a = 5;
+Console.WriteLine(Math.Pow(a,2));
 
 // Третье решение от преподавателя (ввод с клавиатуры)
 double a;
---
+++ Task01.Intern/Program.cs
@@ -0,0 +1,11 @@
+﻿// ЗАДАЧА 01. По двум заданным числам проверять является ли первое квадратом второго
+string numbers (int a, int b)
+{
+    string result = "а^2 не равно b";
+    if (a == b*b) result = "a^2 равно b";
+    return result;
+}
+
+int a = 28;
+int b = 5;
+Console.WriteLine(numbers(a, b));
