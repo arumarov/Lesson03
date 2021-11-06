a909d761ca6c448eaca0993136ba2f776cc4da4c
Arumarov <arumarov@yandex.ru>
2021-11-06 22:27:53 +0600

    Add Task04

---
+++ Task04.Intern/Program.cs
@@ -0,0 +1,13 @@
+﻿// ЗАДАЧА 04. Найти максимальное из трех чисел
+int max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result; 
+}
+int arg1 = 459;
+int arg2 = 356;
+int arg3 = 62;
+
+Console.WriteLine(max(arg1,arg2,arg3));
