166982b4e2d3a25808bd4cb5912ea331e9f30dca
Arumarov <arumarov@yandex.ru>
2021-11-06 21:53:24 +0600

    Add Task15

---
+++ Task15.Junior/Program.cs
@@ -0,0 +1,11 @@
+﻿// ЗАДАЧА 15. Дано число. Проверить кратно ли оно 7 и 23
+
+string multi (int a)
+{
+    string result = "не кратно";
+    if (a%7 == 0 && a%23 == 0) result = "кратно";
+    return result;
+}
+
+int a = 162;
+Console.WriteLine(multi(a));
