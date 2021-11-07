1d9c9b9db71cc17f63b2b32bcb7891cb1ef8acd1
Arumarov <arumarov@yandex.ru>
2021-11-07 21:04:27 +0600

    Add Task07

---
+++ Task07.Intern/Program.cs
@@ -0,0 +1,19 @@
+﻿// ЗАДАЧА 07. Показать числа от -N до N
+int[] array = new int[11];
+
+int index = 0;
+int N = 5;
+
+while (index < 11)
+{
+    array[index] = (N - 2*N + index);
+    index++;
+}
+
+index = 0;
+
+while (index < 11)
+{
+    Console.Write(array[index] + " ");
+    index++;
+}
