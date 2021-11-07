21c76b96cc51d5bc4087a16d6e2326e75697c26b
Arumarov <arumarov@yandex.ru>
2021-11-07 20:32:41 +0600

    Add Task06

---
+++ Task06.Intern/Program.cs
@@ -0,0 +1,10 @@
+﻿// ЗАДАЧА 06. Выяснить является ли число чётным
+string even (int a)
+{
+    string result = "нечетное число";
+    if (a%2 == 0) result = "четное число";
+    return result;
+}
+
+int a = 6;
+Console.WriteLine(even(a));
