dbce95fcdadab7a361647190995bf8b8e0b76f45
Arumarov <arumarov@yandex.ru>
2021-11-07 21:52:42 +0600

    Add Task08

---
+++ Task08.Intern/Program.cs
@@ -0,0 +1,23 @@
+﻿// ЗАДАЧА 08. Показать четные числа от 1 до N
+int[] array = new int[10];
+
+int index = 0;
+int N = 10;
+
+while (index < 10)
+{
+    array[index] = (1 + index);
+    index++;
+}
+
+index = 0;
+
+while (index < 10)
+{
+    if(array[index]%2 == 0) 
+    {
+        Console.Write(array[index] + " ");
+    }
+
+    index++; 
+}
