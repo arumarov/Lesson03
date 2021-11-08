44c7e2ed06942b84af92ecdda24e1c355ccde371
Arumarov <arumarov@yandex.ru>
2021-11-08 14:26:06 +0600

    Add Task31

---
+++ Task31.Senior/Program.cs
@@ -0,0 +1,13 @@
+﻿// ЗАДАЧА 31. Задать массив из 8 элементов и вывести их на экран
+int[] array = {4, 8, 34, 23, 71, 96, 4, 7};
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(array);
