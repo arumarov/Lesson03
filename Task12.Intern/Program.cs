3ae121f4010fcc80ae5484c29df17da8822f7696
Arumarov <arumarov@yandex.ru>
2021-11-09 22:46:46 +0600

    Add Task12

---
+++ Task12.Intern/Program.cs
@@ -0,0 +1,11 @@
+﻿// ЗАДАЧА 12. Удалить вторую цифру трёхзначного числа
+
+int Remove(int a)
+{
+    Console.WriteLine("Введите трехзначное число:");
+    a = Convert.ToInt32(Console.ReadLine());
+    int result = ((a/100%10)*10 + a%10);
+    return result;
+}
+
+Console.WriteLine(Remove(0));
