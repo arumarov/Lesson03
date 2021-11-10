512dcc5b4aaa10f016592abc568749fe3b1b3b98
Arumarov <arumarov@yandex.ru>
2021-11-10 09:46:48 +0600

    Add Task14

---
+++ Task14.Intern/Program.cs
@@ -0,0 +1,13 @@
+﻿// ЗАДАЧА 14. Найти третью цифру числа или сообщить, что её нет
+
+int Find3(int a)
+{
+    int result;
+    Console.WriteLine("Введите трехзначное число:");
+    a = Convert.ToInt32(Console.ReadLine());
+    if (a < 100) result = -1;
+    else result = (a%10);
+    return result;
+}
+
+Console.WriteLine(Find3(0));
