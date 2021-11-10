e18224672eea3f656c44341dfaff82d89d725aa7
Arumarov <arumarov@yandex.ru>
2021-11-10 23:29:59 +0600

    Add Task27

---
+++ Task27.Middle/Program.cs
@@ -0,0 +1,8 @@
+﻿// ЗАДАЧА 27. Определить количество цифр в числе
+
+// Мое решение
+string a;
+Console.Write("Введите целое число: ");
+a = Console.ReadLine();
+Console.Write("Количество цифр в числе: ");
+Console.WriteLine(a.Length);
