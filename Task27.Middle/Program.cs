86c3bb736d2db15d57b0e9089fe93bb293e88daf
Arumarov <arumarov@yandex.ru>
2021-11-11 00:13:03 +0600

    Add Task27

--- Task27.Middle/Program.cs
+++ Task27.Middle/Program.cs
@@ -1,8 +1,25 @@
 ﻿// ЗАДАЧА 27. Определить количество цифр в числе
 
-// Мое решение
-string a;
-Console.Write("Введите целое число: ");
-a = Console.ReadLine();
-Console.Write("Количество цифр в числе: ");
-Console.WriteLine(a.Length);
+// // Мое решение
+// string a;
+// Console.Write("Введите целое число: ");
+// a = Console.ReadLine();
+// Console.Write("Количество цифр в числе: ");
+// Console.WriteLine(a.Length);
+
+
+// Решение от преподавателя (с методом)
+int CountDigit(int n)
+{
+    int k=0;
+
+    while (n!=0)
+    {
+        k++;
+        n=n/10;
+    }
+
+    return k;
+}
+
+Console.WriteLine(CountDigit(456));
