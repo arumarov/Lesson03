40f7195173bbd151b167ce7009a5e2597ec5e3fa
Arumarov <arumarov@yandex.ru>
2021-11-11 19:45:39 +0600

    Add Task31

--- Task31.Senior/Program.cs
+++ Task31.Senior/Program.cs
@@ -1,13 +1,24 @@
 ﻿// ЗАДАЧА 31. Задать массив из 8 элементов и вывести их на экран
-int[] array = {4, 8, 34, 23, 71, 96, 4, 7};
+// Мое решение
+// int[] array = {4, 8, 34, 23, 71, 96, 4, 7};
 
-void PrintArray (int[] array)
-{
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-}
+// void PrintArray (int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+// }
 
-PrintArray(array);
+// PrintArray(array);
+
+
+// Решение от преподавателя
+int[] a = new int[8];
+Random random = new Random();
+for(int i=0; i<a.Length; i++)
+    a[i]=random.Next(0,100);
+
+for(int i=0; i<a.Length; i++)
+    Console.Write(a[i]+" ");
