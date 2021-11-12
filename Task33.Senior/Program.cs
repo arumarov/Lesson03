43e7a6045f2d9d429fa2c5a902c00160bcc71901
Arumarov <arumarov@yandex.ru>
2021-11-12 20:56:53 +0600

    Add Task33

---
+++ Task33.Senior/Program.cs
@@ -0,0 +1,53 @@
+﻿// ЗАДАЧА 33. Задать массив из 12 элементов, заполненных числами из [0,9]. Найти сумму положительных/отрицательных элементов массива
+
+int[] array = new int[12];
+// Console.Write("Задайте число от 1 до 20: " );
+// int a = Convert.ToInt32(Console.ReadLine());
+
+void CreateArray (int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+}
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int SumPositive (int[] array)
+{
+    int count = array.Length;
+    int result = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (array[i] > 0) result = result+array[i];
+    }
+    return result;
+}
+
+int SumNegative (int[] array)
+{
+    int count = array.Length;
+    int result = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (array[i] < 0) result = result+array[i];
+    }
+    return result;
+}
+
+CreateArray(array);
+Console.Write("Заданный массив: ");
+PrintArray(array);
+Console.Write("\n Сумма положительных элементов: ");
+Console.Write(SumPositive(array));
+Console.Write("\n Сумма отрицательных элементов: ");
+Console.Write(SumNegative(array));
