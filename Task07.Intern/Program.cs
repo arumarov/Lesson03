2397aff86a2eb545ae3579decd33fbad33305162
Arumarov <arumarov@yandex.ru>
2021-11-12 19:09:11 +0600

    Add Task07

--- Task07.Intern/Program.cs
+++ Task07.Intern/Program.cs
@@ -1,19 +1,32 @@
 ﻿// ЗАДАЧА 07. Показать числа от -N до N
-int[] array = new int[11];
 
-int index = 0;
-int N = 5;
+// Мое решение
+// int[] array = new int[11];
 
-while (index < 11)
+// int index = 0;
+// int N = 5;
+
+// while (index < 11)
+// {
+//     array[index] = (N - 2*N + index);
+//     index++;
+// }
+
+// index = 0;
+
+// while (index < 11)
+// {
+//     Console.Write(array[index] + " ");
+//     index++;
+// }
+
+
+// Решение с семинара
+Console.WriteLine("Введите число от 1 до 10:");
+string s = Console.ReadLine();
+int b = Convert.ToInt32(s);
+
+for (int i=-b; i<=b; i++)
 {
-    array[index] = (N - 2*N + index);
-    index++;
-}
-
-index = 0;
-
-while (index < 11)
-{
-    Console.Write(array[index] + " ");
-    index++;
+    Console.WriteLine(i);
 }
