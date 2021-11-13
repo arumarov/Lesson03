1655410f45c3e429906a25a9dfc53909efadb337
Arumarov <arumarov@yandex.ru>
2021-11-13 22:46:29 +0600

    Add Task13

---
+++ Task13.Intern/Program.cs
@@ -0,0 +1,22 @@
+﻿// ЗАДАЧА 13. Выяснить, кратно ли число заданному, если нет, вывести остаток
+Console.WriteLine("Введите любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число, на кратность которому проверяем предыдущее число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+string Multiple (int a, int b)
+{
+    string result = String.Empty;
+    if (a%b == 0) 
+    {
+        result = $"{a}" + " кратно " + $"{b}";
+    }
+    else
+    {
+        result = $"{a}" + " не кратно " + $"{b}" +  ", остаток равен " +  $"{a%b}";
+    }
+        
+    return result;
+}
+
+Console.WriteLine(Multiple(a,b));
