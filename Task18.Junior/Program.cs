72cbbbe322e2364e256c978ca60b224813a50f48
Arumarov <arumarov@yandex.ru>
2021-11-13 23:55:08 +0600

    Add Task18

---
+++ Task18.Junior/Program.cs
@@ -0,0 +1,13 @@
+﻿// ЗАДАЧА 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+bool x = false;
+bool y = false;
+bool a = !(x || y);
+bool b = !x && !y;
+if (a == b)
+{
+    Console.WriteLine("Утверждение ¬(X ⋁ Y) = ¬X ⋀ ¬Y - истинно");
+} 
+else
+{
+    Console.WriteLine("Утверждение ¬(X ⋁ Y) = ¬X ⋀ ¬Y - ложно");
+} 
