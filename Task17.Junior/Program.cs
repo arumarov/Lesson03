b69c1e6bfeb60ce6d3a6661e190e66dedaf398c4
Arumarov <arumarov@yandex.ru>
2021-11-15 20:44:32 +0600

    Add Task17

---
+++ Task17.Junior/Program.cs
@@ -0,0 +1,21 @@
+﻿// ЗАДАЧА 17. По двум заданным числам проверять является ли одно квадратом другого
+
+string Check(int a, int b)
+{
+    string result = "Числа A и B не являются квадратами друг друга";
+    if (a == b*b) 
+    {
+        result = "Число B является квадратом числа A";
+    }
+    if (b == a*a)
+    {
+        result = "Число A является квадратом числа B";
+    }
+    return result;
+}
+
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Check(a,b));
