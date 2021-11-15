45202e85b89afe812c61c03b9a5ac2f8c3b59dee
Arumarov <arumarov@yandex.ru>
2021-11-15 21:22:39 +0600

    Add Task21

---
+++ Task21.Junior/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 21. Программа проверяет пятизначное число на палиндром
+
+string Palindrom (int a)
+{
+    string result = ($"{a}" + " не является палиндромом");;
+    if (a/10000%10==a%10 && a/1000%10==a/10%10)
+    {
+        result = ($"{a}" + " является палиндромом");
+    }
+    return result;
+}
+
+Console.WriteLine("Введите пятизначное положительное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Palindrom(a));
