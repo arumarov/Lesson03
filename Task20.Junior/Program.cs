2335fe521250ad072e0ce6de5fb86b0715a5ea6c
Arumarov <arumarov@yandex.ru>
2021-11-15 22:28:08 +0600

    Add Task20

---
+++ Task20.Junior/Program.cs
@@ -0,0 +1,14 @@
+﻿// ЗАДАЧА 20. Ввести номер четверти, показать диапазоны для возможных координат
+string Quarter(int number)
+{
+string result = String.Empty;
+if (number==1) result = "Диапазоны координат: X>0; Y>0";
+if (number==2) result = "Диапазоны координат: X<0; Y>0";
+if (number==3) result = "Диапазоны координат: X<0; Y<0";
+if (number==4) result = "Диапазоны координат: X>0; Y<0";
+return result;
+}
+
+Console.Write("Введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Quarter(number));
