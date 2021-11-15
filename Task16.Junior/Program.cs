5e42e6cb13186fba94a0f87b769c5aa46122f4b7
Arumarov <arumarov@yandex.ru>
2021-11-15 20:10:15 +0600

    Add Task16

---
+++ Task16.Junior/Program.cs
@@ -0,0 +1,16 @@
+﻿// ЗАДАЧА 16. Дано число обозначающее день недели. Выяснить является ли номер выходным днём
+string[] array = {"неделя", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+
+string FindDay (int a)
+{
+    string result = $"{array[a]}" + " - выходной день";
+    if (a<=5) 
+    {
+        result=$"{array[a]}" + " - будний день";
+    }
+    return result;
+}
+
+Console.Write("Введите номер дня недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FindDay(a));
