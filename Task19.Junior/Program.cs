fba657f667e7c3e01eae196d2be96976a3bc7a56
Arumarov <arumarov@yandex.ru>
2021-11-15 22:15:36 +0600

    Add Task19

---
+++ Task19.Junior/Program.cs
@@ -0,0 +1,16 @@
+﻿// ЗАДАЧА 19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+string Quarter(int x, int y)
+{
+    string result = String.Empty;
+    if (x>0 && y>0) result = "Номер четверти: 1";
+    if (x<0 && y>0) result = "Номер четверти: 2";
+    if (x<0 && y<0) result = "Номер четверти: 3";
+    if (x>0 && y<0) result = "Номер четверти: 4";
+    return result;
+}
+
+Console.Write("Введите значение x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Quarter(x,y));
