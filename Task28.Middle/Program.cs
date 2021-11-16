bda7900b39d9098d28802b16751386865c2fd600
Arumarov <arumarov@yandex.ru>
2021-11-16 23:38:51 +0600

    Add Task28

---
+++ Task28.Middle/Program.cs
@@ -0,0 +1,19 @@
+﻿// ЗАДАЧА 28. Подсчитать сумму цифр в числе
+
+int SumDigits(int n)
+{
+    int b=0;
+
+    while (n!=0)
+    {
+        b=b+n%10;
+        n=n/10;
+    }
+
+    return b;
+}
+
+Console.WriteLine("Введите положительное целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Сумма цифр в числе: ");
+Console.Write(SumDigits(n));
