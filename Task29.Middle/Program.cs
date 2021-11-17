e5902df16b4a2aa07ac8bd7f9d32641f3e17eb7a
Arumarov <arumarov@yandex.ru>
2021-11-17 19:26:07 +0600

    Add Task29

---
+++ Task29.Middle/Program.cs
@@ -0,0 +1,14 @@
+﻿// ЗАДАЧА 29. Написать программу вычисления произведения чисел от 1 до N
+int composition(int n)
+{
+    int result = 0;
+    for (int i = 1; i<n; i++)
+    {
+        result = i*n;
+    }
+    return result;
+}
+
+Console.Write("Введите целое число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Произведение чисел от 1 до N: " + $"{composition(n)}");
