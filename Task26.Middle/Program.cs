7b832318cfeeaa926f86b721256edbf1befaa513
Arumarov <arumarov@yandex.ru>
2021-11-17 20:13:12 +0600

    Add Task26

---
+++ Task26.Middle/Program.cs
@@ -0,0 +1,12 @@
+﻿// ЗАДАЧА 26. Возведите число А в натуральную степень B используя цикл
+int degree(int a, int b)
+{
+    int result = a*b;
+    return result;
+}
+
+Console.Write("Введите целое число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число A в степени B: " + $"{degree(a,b)}");
