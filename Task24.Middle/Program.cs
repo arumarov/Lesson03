083815c8f0bbb1310eff857835d857e459ff1453
Arumarov <arumarov@yandex.ru>
2021-11-17 22:16:34 +0600

    Add Task24

---
+++ Task24.Middle/Program.cs
@@ -0,0 +1,38 @@
+﻿// ЗАДАЧА 24. Найти кубы чисел от 1 до N
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+void CreateArray (int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = i+1;
+    }
+}
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void FindCube (int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}" + ":" + $"{array[i]*array[i]*array[i]}" + " ");
+        
+    }
+}
+
+CreateArray(array);
+Console.WriteLine("Заданный массив: ");
+PrintArray(array);
+Console.WriteLine("\nМассив с кубами чисел: ");
+FindCube(array);
