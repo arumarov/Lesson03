d67458178e1697cb5f5055c92b0413b5a31f3160
Arumarov <arumarov@yandex.ru>
2021-11-17 20:50:45 +0600

    Add Task25

---
+++ Task25.Middle/Program.cs
@@ -0,0 +1,11 @@
+﻿// ЗАДАЧА 25. Найти сумму чисел от 1 до А
+int summa(int a)
+{
+    int result = 0;
+    result = a*(a+1)/2;
+    return result;
+}
+
+Console.Write("Введите целое число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Сумма чисел от 1 до A: " + $"{summa(a)}");
