6e64531447f69d0fed1b8b393bcd50b80a5238d6
Arumarov <arumarov@yandex.ru>
2021-12-01 21:24:36 +0600

    Add Task43

---
+++ Task43.Lead/Program.cs
@@ -0,0 +1,10 @@
+﻿// ЗАДАЧА 43.Написать программу преобразования десятичного числа в двоичное
+void Transformation (int number10)
+{
+	string number2=Convert.ToString(number10, 2);
+	Console.WriteLine("В двоичной системе счисления:" + number2);
+}
+
+Console.WriteLine("Введите десятичное число: ");
+int number10 = Convert.ToInt32(Console.ReadLine());
+Transformation(number10);
