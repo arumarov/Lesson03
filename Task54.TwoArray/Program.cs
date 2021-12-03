035946489a7e44bdd3c4c0d03a4f4e0420d6acb9
Arumarov <arumarov@yandex.ru>
2021-12-03 21:28:09 +0600

    Add Task54

---
+++ Task54.TwoArray/Program.cs
@@ -0,0 +1,50 @@
+﻿// ЗАДАЧА 54. В матрице чисел найти сумму элементов главной диагонали
+ 
+Random rnd = new Random();
+void SetArray2D (int[,] a)
+{ 
+    for(int i=0;i<a.GetLength(0);i++)
+    { 
+        for(int j=0;j<a.GetLength(1);j++)
+        { 
+            a[i,j] = rnd.Next(1,101);         
+        } 
+    } 
+}
+ 
+void PrintArray(int[,] a)
+{ 
+    for(int i=0;i<a.GetLength(0);i++)
+    { 
+        for(int j=0;j<a.GetLength(1);j++)
+        { 
+            Console.Write($"{a[i,j],4}");         
+        }
+        Console.WriteLine(); 
+    } 
+}
+ 
+int FindSumDia(int[,] a)
+{
+    int temp = 0;
+    for(int i=0;i<a.GetLength(0);i++)
+    { 
+        for(int j=0;j<a.GetLength(1);j++)
+        { 
+            if(i==j) temp = temp+a[i,j];     
+        } 
+    }
+    return temp;
+}
+ 
+ 
+Console.WriteLine("Введите длину стороны массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = m;
+int[,] a;
+a = new int[m,n];
+SetArray2D(a);
+Console.WriteLine("Массив: ");
+PrintArray(a);
+FindSumDia(a);
+Console.WriteLine("Сумма элементов главной диагонали: " + FindSumDia(a));
