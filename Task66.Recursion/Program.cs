ac8a48d6e031838d2c8bbdc713ebbb9324cc7848
Arumarov <arumarov@yandex.ru>
2021-12-04 21:30:43 +0600

    Add Task66

---
+++ Task66.Recursion/Program.cs
@@ -0,0 +1,15 @@
+﻿// ЗАДАЧА 66. Показать натуральные числа от 1 до N, N задано
+
+int ShowNumbers(int n)
+{
+    if (n==1) return 1;
+    else return n*ShowNumbers(1);
+}
+
+for(int i=1;i<=12;i++)
+{
+    Console.Write(ShowNumbers(i) + " ");
+}
+
+int n=12;
+ShowNumbers(n);
