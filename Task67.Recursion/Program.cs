6c2b9c7f261a5bc818c4c3c7e470a7514fbdcb04
Arumarov <arumarov@yandex.ru>
2021-12-04 21:41:44 +0600

    Add Task67

---
+++ Task67.Recursion/Program.cs
@@ -0,0 +1,15 @@
+﻿// ЗАДАЧА 67. Показать натуральные числа от N до 1, N задано
+
+int ShowNumbers(int n)
+{
+    if (n>1) return n*ShowNumbers(1);
+    else return 1;
+}
+
+for(int i=15;i>=1;i--)
+{
+    Console.Write(ShowNumbers(i) + " ");
+}
+
+int n=15;
+ShowNumbers(n);
