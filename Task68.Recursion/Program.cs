7683efbb46b87ff203a4ea6dbbb2639003df7096
Arumarov <arumarov@yandex.ru>
2021-12-04 22:09:47 +0600

    Add Task68

---
+++ Task68.Recursion/Program.cs
@@ -0,0 +1,16 @@
+﻿// ЗАДАЧА 68. Показать натуральные числа от M до N, N и M заданы
+
+int ShowNumbers(int m)
+{
+    int n=20;
+    if (m<=n) return m;
+    else return ShowNumbers(m);
+}
+
+for(int i=10;i<=20;i++)
+{
+    Console.Write(ShowNumbers(i) + " ");
+}
+
+int m=20;
+ShowNumbers(m);
