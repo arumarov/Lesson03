30740eb8f4aa7243d96e5636ef8086f4a563b012
Arumarov <arumarov@yandex.ru>
2021-12-07 21:50:12 +0600

    Add Task69

---
+++ Task69.Recursion/Program.cs
@@ -0,0 +1,11 @@
+﻿// ЗАДАЧА 69. Найти сумму элементов от M до N, N и M заданы
+
+int SumElements(int m, int n)
+{
+    if (m>n) return 0;
+    else return m+SumElements(m+1,n);
+}
+
+int m=10;
+int n=15;
+Console.WriteLine("Сумма элементов от " + $"{m}" + " до " + $"{n} : " + $"{SumElements(m,n)}");
