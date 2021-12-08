8aab2ce427accf570ab50b1842d531fa1cb151b8
Arumarov <arumarov@yandex.ru>
2021-12-08 21:20:01 +0600

    Add Task70

---
+++ Task70.Recursion/Program.cs
@@ -0,0 +1,10 @@
+﻿// ЗАДАЧА 70. Найти сумму цифр числа
+
+int SumNumbers(int a)
+{
+    if (a!<1) return 0;
+    else return ((a+1/10)%10)+SumNumbers(a/10);
+}
+
+int a = 22222;
+Console.WriteLine("Сумма цифр числа " + $"{a}: " + $"{SumNumbers(a)}");
