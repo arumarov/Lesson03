95dd17e90652ea07b21af734b73541175302b63b
Arumarov <arumarov@yandex.ru>
2021-12-08 22:07:27 +0600

    Add Task72

---
+++ Task72.Recursion/Program.cs
@@ -0,0 +1,10 @@
+﻿// ЗАДАЧА 72. Написать программу возведения числа А в целую степень B
+int Degree(int a, int b)
+{
+    if (b == 0) return 1;
+    else return a * Degree(a,b-1);
+}
+
+int a = 2;
+int b = 4;
+Console.WriteLine(Degree(a, b));
