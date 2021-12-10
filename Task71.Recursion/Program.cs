a6cbcc82934cecac881694d33dc0e64531e81fdf
Arumarov <arumarov@yandex.ru>
2021-12-10 22:23:10 +0600

    Add Task71

---
+++ Task71.Recursion/Program.cs
@@ -0,0 +1,11 @@
+﻿// ЗАДАЧА 71. Написать программу вычисления функции Аккермана
+//Мое решение
+int Akkerman(int m, int n)
+{
+    if(m==0) return n+1;
+    if(m>0 && n==0) return Akkerman(m-1,1);
+    if(m>0 && n>0) return Akkerman(m-1,Akkerman(m,n-1));
+    else return 0;
+}
+
+Console.WriteLine(Akkerman(0,0));
